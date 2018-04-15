16c3bd804d5eb7ace8079a581c200abb3eaebd72
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2018-04-14 22:28:19 -0400

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< HEAD
   title: string = 'google maps';
   lat: number = 41.6377623;
   lng: number = -70.9273704;
 }
+=======
+  title = 'app';
+}
+>>>>>>> parent of 367d31a... 'AngularApp'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+
 import { AppComponent } from './app.component';
-import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    BrowserModule,
-    CommonModule,
-    FormsModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDoFleQCO6FHdDje1Kbgq5Q_Kfd_4n5yso'
-    })
+    BrowserModule
   ],
   providers: [],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
